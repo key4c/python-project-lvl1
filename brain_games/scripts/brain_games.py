7c44a10ce818632768ba887df9f699bb00d9649c
key4c <key4citizen@gmail.com>
2022-08-15 14:51:17 +0300

    add input name and greeting

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+import prompt
+
 def brain_games():
     print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f'"Hello, "{name}')
 
 def main():
     brain_games()
