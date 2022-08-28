79918b6ed9d382b5cc53d9b0f5b6d790fcc7a97d
key4c <key4citizen@gmail.com>
2022-08-28 23:37:09 +0300

    corrected logic of 3 correct answers

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -8,18 +8,20 @@ def even_game():
     print(
         f'Hello, {user_name}!\
         \nAnswer "yes" if the number is even, otherwise answer "no".')
-    i = 1
+    i = 0
     while i <= 3:
         random_number = randint(1, 100)
         print(f'Question: {random_number}')
         user_answer = str(input('Your answer: '))
         correct_answer = ('no' if random_number % 2 != 0 else 'yes')
         if user_answer != correct_answer:
-            print(f'{user_answer} is wrong answer ;(. \
-                    Correct answer was {correct_answer}.')
+            print(f'{user_answer} is wrong answer ;(.'
+                  f' Correct answer was {correct_answer}.')
             print(f'Let\'s try again, {user_name}')
             break
         else:
             print("Correct!")
             i += 1
-        print(f'Congratulations, {user_name}!')
+        if i == 3:
+            print(f'Congratulations, {user_name}!')
+            break
