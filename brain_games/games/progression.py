27bda2ed62ed800d993717dadc1185d48e835058
key4c <key4citizen@gmail.com>
2022-09-04 23:01:30 +0300

    make logic game and add script

---
+++ brain_games/games/progression.py
@@ -0,0 +1,22 @@
+from random import randint, choice
+
+
+GUIDE = 'What number is missing in the progression?'
+
+def generate_round():
+    increment = randint(2, 10)
+    start_number = randint(1, 10)
+
+    question = [start_number]
+    i = 0
+    num = start_number
+    while i < 10:
+        num += increment
+        question.append(num)
+        i += 1
+    correct_answer = choice(question)
+    index_correct_answer = question.index(correct_answer)
+    question[index_correct_answer] = '..'
+
+    return question, str(correct_answer)
+
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.games import progression
+from brain_games import engine
+
+
+def main():
+    engine.start_the_game(progression)
+
+
+if __name__ == '__main__':
+    main()
