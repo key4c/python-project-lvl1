0c64c262ed40701067c87d00d921755a4369ea47
key4c <key4citizen@gmail.com>
2022-09-17 00:01:06 +0300

    changed else to elsif

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,16 +5,16 @@ from random import choice
 GUIDE = 'What is the result of the expression?'
 
 
+first_operator = randint(1, 50)
+second_operand = randint(1, 50)
 def generate_round():
     operators = ['+', '-', '*']
-    first_operand = randint(1, 10)
-    second_operand = randint(1, 10)
     random_operator = choice(operators)
     if random_operator == '+':
         correct_answer = first_operand + second_operand
     elif random_operator == '-':
         correct_answer = first_operand - second_operand
-    else:
+    elif random_operator == '*':
         correct_answer = first_operand * second_operand
     question = f'{first_operand} {random_operator} {second_operand}'
     return question, str(correct_answer)
