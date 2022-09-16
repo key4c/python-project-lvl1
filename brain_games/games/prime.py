593e00cad055cef189452bf704554783c1f2ae8d
key4c <key4citizen@gmail.com>
2022-09-16 23:22:53 +0300

    fixed bug in determining prime number

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,12 +4,19 @@ from random import randint
 GUIDE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+# Воженная функция проверяет является ли число простым.
+def is_prime(random_number):
+    if random_number <= 1:
+        return False
+    else:
+        for i in range(2, random_number // 2 + 1):
+            if random_number % i == 0:
+                return False
+    return True
+
+
 def generate_round():
-    random_number = randint(1, 10)
+    random_number = randint(0, 100)
     question = f'{random_number}'
-    k = 0
-    for i in range(2, random_number // 2 + 1):
-        if (random_number % i == 0):
-            k += 1
-    correct_answer = 'yes' if k <= 0 else "no"
-    return question, correct_answer
+    correct_answer = 'yes' if is_prime(random_number) else 'no'
+    return random_number, correct_answer
